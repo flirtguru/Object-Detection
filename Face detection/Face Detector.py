4e772a5b1a05027d20d33e14bc337e2017fefbfa
Hacker Ninja <30948709+flirtguru@users.noreply.github.com>
2017-11-28 04:12:15 +0530

    Update Face Detector.py

--- Face detection/Face Detector.py
+++ Face detection/Face Detector.py
@@ -2,21 +2,27 @@ import cv2
 import numpy as np
 
 classifier=cv2.CascadeClassifier("Projects/Modules/haarcascade_frontalface_default.xml")
+#importing a pretrained classifier
 
 image=cv2.imread("aunty.jpg")
+#importing our test image
 
 gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+#grayscale conversion
 
 det=classifier.detectMultiScale(gray,1.2,3)
+#returns the top left and bottom right coordinates 
 
-if det is():
+if det is ():
     print("NO FACES FOUND!!")
+    
 
 for (t1,t2,b1,b2) in det :
     
     cv2.rectangle(image,(t1,t2),(t1+b1,t2+b2),(69,69,69),2)
     
     cv2.imshow("Detected ",image)
+    #VOILA!!!!
     
     cv2.waitKey(0)
     
